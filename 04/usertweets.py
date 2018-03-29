d1d2b362352ffd8db01c467eb889b4a0eae092f1
Vaughn Johnson <evilepicproductions@gmail.com>
2018-03-28 23:08:08 -0700

    need some serious refactoring. Also does not pass unittests

---
+++ 04/usertweets.py
@@ -0,0 +1,50 @@
+from collections import namedtuple
+import csv
+import os
+
+import tweepy
+
+from config import CONSUMER_KEY, CONSUMER_SECRET
+from config import ACCESS_TOKEN, ACCESS_SECRET
+
+DEST_DIR = 'data'
+EXT = 'csv'
+NUM_TWEETS = 100
+
+
+class UserTweets(object):
+	"""TODOs:
+	- create a tweepy api interface
+	- get all tweets for passed in handle
+	- optionally get up until 'max_id' tweet id
+	- save tweets to csv file in data/ subdirectory
+	- implement len() an getitem() magic (dunder) methods"""
+
+	def __init__(self, handle, max_id=None):
+		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+		api = tweepy.API(auth)
+
+		TWEET_COUNT = 100
+
+		self.recent_tweets = api.user_timeline(handle, count=TWEET_COUNT)
+		
+		with open("data/some_handle.csv", 'w') as csvfile:
+			writer = csv.writer(csvfile)
+			for tw in self.recent_tweets:
+				writer.writerow([tw.id_str, tw.created_at, tw.text])
+
+	def __len__(self):
+		return len(self.recent_tweets)
+
+	def __getitem__(self, pos):
+		return self.recent_tweets[pos]	
+
+if __name__ == "__main__":
+
+	for handle in ('pybites', 'techmoneykids', 'bbelderbos'):
+	    print('--- {} ---'.format(handle))
+	    user = UserTweets(handle)
+	    for tw in user[:5]:
+	        print(tw)
+	    print()
